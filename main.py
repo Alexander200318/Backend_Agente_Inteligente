8e80e920d289c52f8d42903f3ed2e2cd4506b093
JhonyMendez <155590013+JhonyMendez@users.noreply.github.com>
2025-12-01 18:51:23 -0500

    Actualizado

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def add_security_headers(request: Request, call_next):
     
     # Headers de seguridad
     response.headers["X-Content-Type-Options"] = "nosniff"
-    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-Frame-Options"] = "ALLOWALL"
     response.headers["X-XSS-Protection"] = "1; mode=block"
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
